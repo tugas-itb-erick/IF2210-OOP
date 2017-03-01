d5d85576f5f9582e0f2705b198d08e800395a7f7
sutantowilliam <willywilliamss97@gmail.com>
2017-03-01 21:08:03 +0700

    bingung isinya

--- Diet.h
+++ Diet.h
@@ -1,8 +1,26 @@
-/* NIM/Nama  : 13515AAA / AAA AAA   */
-/* File      : AAA.h                */
+/* NIM/Nama  : 13515144 / William   */
+/* File      : Diet.h                */
 
 #pragma once
 
+/** @class Carnivore
+  * Kelas untuk hewan karnivora (pemakan daging)
+  */
 class Carnivore {
+public:
+  
+};
 
+/** @class Herbivore
+  * Kelas untuk hewan herbivora (pemakan tanaman)
+  */
+class Herbivore{
+public:  
+};
+
+/** @class Omnivore
+  * Kelas untuk hewan omnivora (pemakan segala)
+  */
+class Omnivore{
+public:
 };
--- Taxonomy.h
+++ Taxonomy.h
@@ -1,8 +1,39 @@
-/* NIM/Nama  : 13515AAA / AAA AAA   */
-/* File      : AAA.h                */
+/* NIM/Nama  : 13515144 / William   */
+/* File      : Taxonomy.h                */
 
 #pragma once
 
-class Mammals {
+/** @class Mammalia
+  * Kelas taksonomi untuk hewan menyusui (mamalia)
+  */
+class Mammalia {
+
+};
+
+/** @class Amphibia
+  * Kelas taksonomi untuk amphibi
+  */
+class Amphibia {
+
+};
+
+/** @class Reptilia
+  * Kelas taksonomi untuk reptil
+  */
+class Reptilia {
+
+};
+
+/** @class Pisces
+  * Kelas taksonomi untuk ikan
+  */
+class Pisces {
+
+};
+
+/** @class Aves
+  * Kelas taksonomi untuk burung
+  */
+class Aves {
 
 };
