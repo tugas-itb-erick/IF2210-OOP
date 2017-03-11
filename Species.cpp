15724118c4704907d23bae67831f75dc858109d3
sutantowilliam <willywilliamss97@gmail.com>
2017-03-11 16:54:48 +0700

    Add files via upload

---
+++ Species.cpp
@@ -0,0 +1,101 @@
+#include "Species.h"
+#include "Color.h"
+#include <iostream>
+using namespace std;
+
+
+void Elephant::interact(){
+	cout<< "This elephant is trumpeting!!" << endl;
+}
+
+void Elephant::render(){
+	cout<<FWHT("E")<<endl;
+}
+
+void Girrafe::interact(){
+	cout<< "This girrafe is eating high tree leaves"
+}
+
+void Elephant::render(){
+	cout<<""<<endl;
+}
+
+void Lion::interact(){
+	cout<< "Roarrrr..!!"
+}
+
+void Elephant::render(){
+	cout<<""<<endl;
+}
+
+void Tiger::interact(){
+	cout<< "Grrrr...!! Grrr...!!"
+}
+
+void Elephant::render(){
+	cout<<""<<endl;
+}
+
+void Orangutan::interact(){
+	cout<< "This orangutan is playing on a tree!"
+}
+
+void Elephant::render(){
+	cout<<""<<endl;
+}
+
+void Chimpanzee::interact(){
+	cout<< "U u a a ... U u a a"
+}
+
+void Elephant::render(){
+	cout<<""<<endl;
+}
+
+void Komodo::interact(){
+	cout<< "So amazing komodo!!"
+}
+
+void Elephant::render(){
+	cout<<""<<endl;
+}
+
+void Bear::interact(){
+	cout<< "Growl.. Growl.."
+}
+
+void Elephant::render(){
+	cout<<""<<endl;
+}
+
+void Frog::interact(){
+	cout<< "Croag.... Croag.... Ribbet... Ribbet..."
+}
+
+void Elephant::render(){
+	cout<<""<<endl;
+}
+
+void Alligator::interact(){
+	cout<< "Hiss....."
+}
+
+void Elephant::render(){
+	cout<<""<<endl;
+}
+
+void Hippopotamus::interact(){
+	cout<< "This hippo is so big"
+}
+
+void Elephant::render(){
+	cout<<""<<endl;
+}
+
+void Turtle::interact(){
+	cout<< "This turtle is walking slowly"
+}
+
+void Elephant::render(){
+	cout<<""<<endl;
+}
