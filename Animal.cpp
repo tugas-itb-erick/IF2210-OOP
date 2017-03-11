8d9ef4f627db4b04290c85fbe39485197cb48967
sutantowilliam <willywilliamss97@gmail.com>
2017-03-11 20:24:56 +0700

    *

---
+++ Animal.cpp
@@ -0,0 +1,61 @@
+/* NIM/Nama  : 13515144 / Erick Wijaya */
+/* File      : Animal.h                */
+
+#include <iostream>
+using namespace std;
+#include "Animal.h"
+
+Animal::Animal(){
+	name=" ";
+	weight=0;
+	s=male;	
+}
+
+Animal::Animal(string _name, double _weight, Sex _s){
+	name=_name;
+	weight=_weight;
+	s=_s;
+}
+
+Animal::Animal(const Animal& A){
+	name=A.name;
+	weight=A.weight;
+	s=A.s;
+}
+
+Animal::~Animal(){
+}
+
+Animal& Animal::operator=(const Animal& A){
+	name=A.name;
+	weight=A.weight;
+	s=A.s;
+	return *this;
+}
+
+LandAnimal::LandAnimal(){
+}
+LandAnimal::LandAnimal(const Animal&){
+}
+LandAnimal::~LandAnimal(){
+}
+LandAnimal& LandAnimal::operator=(const LandAnimal&){
+}
+
+WaterAnimal::WaterAnimal(){
+}
+WaterAnimal::WaterAnimal(const Animal&){
+}
+WaterAnimal::~WaterAnimal(){
+}
+WaterAnimal& WaterAnimal::operator=(const WaterAnimal&){
+}
+
+FlyingAnimal::FlyingAnimal(){
+}
+FlyingAnimal::FlyingAnimal(const Animal&){
+}
+FlyingAnimal::~FlyingAnimal(){
+}
+FlyingAnimal& FlyingAnimal::operator=(const FlyingAnimal&){
+}
