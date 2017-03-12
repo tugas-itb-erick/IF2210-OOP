ac5486a849a5c57d423df7a977cd7fb19d1d3d66
Erick Wijaya <wijaya.erick52@gmail.com>
2017-03-13 03:16:15 +0700

    Add files via upload

--- renders/Cage.h
+++ renders/Cage.h
@@ -12,7 +12,7 @@ public:
   /** @brief Constructor
 	*/
   Cage();
-  
+
   /** @brief Constructor dengan parameter.
     * @param s ukuran cage
     */
@@ -23,12 +23,12 @@ public:
 	* @param Cage C yang diacu cctor
 	*/
   Cage(const Cage& C);
-  
-  
+
+
   /** @brief Destructor
 	*/
   ~Cage();
-  
+
   /** @brief Operator=.
 	  * Menjamin bukan bitwise copy.
 	  * @param Cage C berisi data untuk diassign
@@ -40,19 +40,19 @@ public:
 	* @return integer ukuran cage
 	*/
   int getSize() const;
-  
+
   /** @brief GetTotalAnimal.
 	* Mengembalikan banyaknya binatang dalam satu cage.
 	* @return integer banyaknya binatang
-	*/  
+	*/
   int getTotalAnimal() const;
-  
+
    /** @brief GetRow.
 	 * Mengembalikan posisi baris.
 	 * @return posisi baris
-	 */   
+	 */
   int * getRow() const;
-  
+
   /** @brief GetCol.
 	 * Mengembalikan posisi kolom.
 	 * @return posisi kolom
@@ -64,44 +64,46 @@ public:
 	 * @return mengembalikan true/false
 	 */
   bool isFull() const;
-  
+
   /** @brief AddAnimal.
 	 * Menambahkan binatang ke dalam cage
 	 * @param A animal
-	 */  
+	 */
   void AddAnimal(const Animal& A);
 
   /** @brief countConsumedMeat.
 	 * Menghitung jumlah makanan daging
 	 * @return jumlah daging
-	 */ 
+	 */
   int countConsumedMeat();
-  
+
   /** @brief countConsumedVeggie.
 	 * Menghitung jumlah makanan sayuran
 	 * @return jumlah sayuran
-	 */ 
+	 */
   int countConsumedVeggie();
-  
+
   /** @brief render.
-	 */ 
+	 */
   void render();
-  
+
   /** @brief Operator>>.
 	 * Menambahkan binatang ke dalam cage
 	 * @param A animal
-	 */ 
+	 */
   friend istream& operator>>(istream& in, Cage& C);
 
     /** @brief Operator [].
 	 * Operator subscript
 	 * @param x integer indeks
-	 */ 
+	 */
 	 Animal*& operator[](int x);
+>>>>>>> 213b73bd81652b653f7d662408b143eb668811e1
 
 private:
   int * row; // posisi baris
   int * col; // posisi kolom
   Animal * *a;
+  Habitat *h;
   int size;
 };
