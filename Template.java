d559992b04e3d40b482b6eccfcccda47258e01ed
wijayaerick <wijaya.erick52@gmail.com>
2017-03-23 16:55:09 +0700

    Example of Java Class

---
+++ Template.java
@@ -0,0 +1,24 @@
+/* NIM/Nama : 13515xxx / Abc Def */
+/* File : Template.java */
+
+import java.util.*;
+
+// contoh
+public class Template{
+  private int a;
+  private String s;
+  private int x[];
+
+  public Template(){
+    s = new String();
+    a = 0;
+    x = new int[10];
+  }
+  public int getA(){
+    return a;
+  }
+
+}
+
+// kompilasi : javac Template.java
+// run (klo ada main) : java Template
