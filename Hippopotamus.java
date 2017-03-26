b95c88c6e2d1c478abba8168bb3f2fdb590ab9e5
wijayaerick <wijaya.erick52@gmail.com>
2017-03-26 17:39:16 +0700

    AirHabitat.java

--- Hippopotamus.java
+++ Hippopotamus.java
@@ -11,7 +11,7 @@ class Hippopotamus extends Animal implements LandAnimal, WaterAnimal, Herbivore,
   public Hippopotamus() {
     super();
   }
-  /** 
+  /**
    * Consructor dengan parameter
    * @param _name nama binatang
    * @param _weight berat
@@ -31,14 +31,14 @@ class Hippopotamus extends Animal implements LandAnimal, WaterAnimal, Herbivore,
     Hippopotamus out = new Hippopotamus(name,weight,sex,row,col);
     return out;
   }
-  /** 
+  /**
    * Menampilkan interaksi hewan ke layar
    */
   @Override
   public void Interact() {
   	System.out.println("This hippo is so big");
   }
-  /** 
+  /**
    * Mengembalikan karakter id tiap hewan
    * @return karakter tiap hewan
    */
@@ -46,13 +46,13 @@ class Hippopotamus extends Animal implements LandAnimal, WaterAnimal, Herbivore,
   public char Render() {
     return 'H';
   }
-  /** 
+  /**
    * Mengembalikan warna dari hewan
    * @return warna cetak hewan
    */
   @Override
   public String RenderWithColor() {
-    return ANSI_GREEN + render() + ANSI_RESET;
+    return ANSI_GREEN + Render() + ANSI_RESET;
   }
   /**
    * Mengembalikan liar/tidaknya binatang
@@ -62,14 +62,14 @@ class Hippopotamus extends Animal implements LandAnimal, WaterAnimal, Herbivore,
   public boolean IsWild() {
     return true;
   }
-  /** 
+  /**
    * Mengembalikan jumlah daging yang dikonsumsi
    * @return jumlah daging yang dikonsumsi
    */
   public double CountConsumedMeat() {
     return weight*meatRatio;
   }
-  /** 
+  /**
    * Mengembalikan jumlah makanan tumbuhan yang dikonsumsi
    * @return jumlah makanan tumbuhan yang dikonsumsi
    */
