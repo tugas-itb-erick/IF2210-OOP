17c9619fe326c3e0dbfc9e3642e9ddc240a2c0dd
Veren <willywilliamss97@gmail.com>
2017-03-26 15:08:04 +0700

    cell lolos compile

--- Cell.java
+++ Cell.java
@@ -9,7 +9,6 @@ import java.lang.*;
 public class Cell {
   private Place type;
   private char id;
-  private Color color;
   private int m;
   private int n;
 
@@ -17,39 +16,43 @@ public class Cell {
    * Constructor
    */
   public Cell() {
-    type=PLACE_NONE;
-	id='!';
-	color=WHITE;
-	m=0;
-	n=0;
+    type = Place.PLACE_NONE;
+	  id = '!';
+	  m = 0;
+	  n = 0;
   }
   /** 
    * Consructor dengan parameter
    * @param  _type tipe sel 
    * @param _id karakter id sel
-   * @param _color warna sel
    * @param _m baris
    * @param _n kolom
    */
-  public Cell(Place _type, char _id,Color _color, int _m, int _n) {
-    type=_type;
-	id=_id;
-	color=_color;
-	m=_m;
-	n=_n;
+  public Cell(Place _type, char _id, int _m, int _n) {
+    type =_type;
+    id =_id;
+    m =_m;
+    n =_n;
   }	
+  /** 
+   * Mengembalikan karakter id untuk di render
+   * @return karakter current cell
+   */
+  public char GetId() {
+  	return id;
+  }
   /** 
    * Mengembalikan karakter id dari current cell 
    * @return karakter current cell
    */
-  public char getId() {
-  	return id;
+  public char Render() {
+    return GetId();
   }
-  /** @brief getColor
-   * Mengembalikan warna dari current cell
-   * @return warna current cell
+  /** 
+   * Mengembalikan warna dari hewan
+   * @return warna cetak hewan
    */
-  public Color getColor() {
-  	return color;
+  public String RenderWithColor() {
+    return Color.ANSI_WHITE + Render() + Color.ANSI_RESET;
   }
 }
