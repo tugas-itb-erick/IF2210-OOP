d7a68a07e4ca42a1f21f9d24c686eaee2131b123
audrynyonata <audrynyonata@gmail.com>
2017-03-27 23:17:21 +0700

    maap gua delet

---
+++ Vertebrate.java
@@ -0,0 +1,10 @@
+/** 
+ * @author NIM / Nama	: 13515144 / William	
+ *	Nama file	: Vertebrate.java			
+ */
+
+public interface Vertebrate {
+	public char blood_temp;
+	public int heart_chamber;
+	
+}
