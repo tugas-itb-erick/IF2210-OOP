00ab0b2079b4f2b61af8b126700c10e0fd21249d
wijayaerick <wijaya.erick52@gmail.com>
2017-03-29 00:24:54 +0700

    Cage JUnit

---
+++ src/habitat/water/WaterHabitatTest.java
@@ -0,0 +1,15 @@
+package habitat.water;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WaterHabitatTest {
+	WaterHabitat test = new WaterHabitat();
+	
+	@Test
+	public void testRender() {
+		assertTrue(test.render() == 'W');
+	}
+
+}
