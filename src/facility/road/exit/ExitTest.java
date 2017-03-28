f61257c48e7943b15077f53a371399bd9643f96e
wijayaerick <wijaya.erick52@gmail.com>
2017-03-29 01:25:22 +0700

    *

---
+++ src/facility/road/exit/ExitTest.java
@@ -0,0 +1,15 @@
+package facility.road.exit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExitTest {
+	Exit test = new Exit();
+	
+	@Test
+	public void testRender() {
+		assertTrue(test.render() == 'o');
+	}
+
+}
